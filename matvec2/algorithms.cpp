30fff2b47c57fee471f9725c9a1018e25bbf3b78
Damian Malczewski <damian.m.malczewski@gmail.com>
2020-01-23 00:02:39 +0100

    Format code

--- matvec2/algorithms.cpp
+++ matvec2/algorithms.cpp
@@ -2,25 +2,21 @@
 #include <emmintrin.h>
 #include <immintrin.h>
 
-void matvec_naive(double *y_vect, const double *a_matr, const double *x_vect, int size)
+void matvec_naive(double* y_vect, const double* a_matr, const double* x_vect, int size)
 {
-    for (int i = 0; i < size; ++i)
-    {
+    for (int i = 0; i < size; ++i) {
         y_vect[i] = 0.0;
-        for (int j = 0; j < size; ++j)
-        {
+        for (int j = 0; j < size; ++j) {
             y_vect[i] += a_matr[i * size + j] * x_vect[j];
         }
     }
 }
 
-void matvec_sse2(double *y_vect, const double *a_matr, const double *x_vect, int size)
-{}
+void matvec_sse2(double* y_vect, const double* a_matr, const double* x_vect, int size) {}
 
-void matvec_avx(double *y_vect, const double *a_matr, const double *x_vect, int size)
-{}
+void matvec_avx(double* y_vect, const double* a_matr, const double* x_vect, int size) {}
 
-void matvec_fma_avx(double *y_vect, const double *a_matr, const double *x_vect, int size)
+void matvec_fma_avx(double* y_vect, const double* a_matr, const double* x_vect, int size)
 {
     int outer_unwinding = 4;
     int inner_unwinding = 16;
@@ -29,24 +25,21 @@ void matvec_fma_avx(double *y_vect, const double *a_matr, const double *x_vect, 
     __m256d rx0, rx1, rx2, rx3;
     __m256d ra0, ra1, ra2, ra3;
 
-    const double *ptr_a = a_matr;
-    const double *ptr_x = nullptr;
+    const double* ptr_a = a_matr;
+    const double* ptr_x = nullptr;
 
-    double *buf0 = static_cast<double *>(aligned_alloc(32, 4 * sizeof(double)));
-    double *buf1 = static_cast<double *>(aligned_alloc(32, 4 * sizeof(double)));
-    double *buf2 = static_cast<double *>(aligned_alloc(32, 4 * sizeof(double)));
-    double *buf3 = static_cast<double *>(aligned_alloc(32, 4 * sizeof(double)));
+    double* buf0 = static_cast<double*>(aligned_alloc(32, 4 * sizeof(double)));
+    double* buf1 = static_cast<double*>(aligned_alloc(32, 4 * sizeof(double)));
+    double* buf2 = static_cast<double*>(aligned_alloc(32, 4 * sizeof(double)));
+    double* buf3 = static_cast<double*>(aligned_alloc(32, 4 * sizeof(double)));
 
+    memset(static_cast<void*>(y_vect), 0, size * sizeof(double));
 
-    memset(static_cast<void *>(y_vect), 0, size * sizeof(double));
-
-    for (int i = 0; i < size; i += outer_unwinding)
-    {
+    for (int i = 0; i < size; i += outer_unwinding) {
         ry0 = ry1 = ry2 = ry3 = _mm256_setzero_pd();
         ptr_x = x_vect;
 
-        for (int j = 0; j < size; j += inner_unwinding)
-        {
+        for (int j = 0; j < size; j += inner_unwinding) {
             _mm_prefetch(static_cast<const void*>(ptr_x + inner_unwinding), _MM_HINT_T0);
             _mm_prefetch(static_cast<const void*>(ptr_x + inner_unwinding + 8), _MM_HINT_T0);
             _mm_prefetch(static_cast<const void*>(ptr_a + inner_unwinding), _MM_HINT_NTA);
@@ -58,12 +51,10 @@ void matvec_fma_avx(double *y_vect, const double *a_matr, const double *x_vect, 
             _mm_prefetch(static_cast<const void*>(ptr_a + 3 * size + inner_unwinding), _MM_HINT_NTA);
             _mm_prefetch(static_cast<const void*>(ptr_a + 3 * size + inner_unwinding + 8), _MM_HINT_NTA);
 
-
             rx0 = _mm256_load_pd(ptr_x);
             rx1 = _mm256_load_pd(ptr_x + 4);
             rx2 = _mm256_load_pd(ptr_x + 8);
             rx3 = _mm256_load_pd(ptr_x + 12);
-
 
             ra0 = _mm256_load_pd(ptr_a);
             ra1 = _mm256_load_pd(ptr_a + size);
@@ -75,7 +66,6 @@ void matvec_fma_avx(double *y_vect, const double *a_matr, const double *x_vect, 
             ry2 = _mm256_fmadd_pd(ra2, rx0, ry2);
             ry3 = _mm256_fmadd_pd(ra3, rx0, ry3);
 
-
             ra0 = _mm256_load_pd(ptr_a + 4);
             ra1 = _mm256_load_pd(ptr_a + 4 + size);
             ra2 = _mm256_load_pd(ptr_a + 4 + 2 * size);
@@ -85,7 +75,6 @@ void matvec_fma_avx(double *y_vect, const double *a_matr, const double *x_vect, 
             ry1 = _mm256_fmadd_pd(ra1, rx1, ry1);
             ry2 = _mm256_fmadd_pd(ra2, rx1, ry2);
             ry3 = _mm256_fmadd_pd(ra3, rx1, ry3);
-
 
             ra0 = _mm256_load_pd(ptr_a + 8);
             ra1 = _mm256_load_pd(ptr_a + 8 + size);
@@ -97,7 +86,6 @@ void matvec_fma_avx(double *y_vect, const double *a_matr, const double *x_vect, 
             ry2 = _mm256_fmadd_pd(ra2, rx2, ry2);
             ry3 = _mm256_fmadd_pd(ra3, rx2, ry3);
 
-
             ra0 = _mm256_load_pd(ptr_a + 12);
             ra1 = _mm256_load_pd(ptr_a + 12 + size);
             ra2 = _mm256_load_pd(ptr_a + 12 + 2 * size);
@@ -107,7 +95,6 @@ void matvec_fma_avx(double *y_vect, const double *a_matr, const double *x_vect, 
             ry1 = _mm256_fmadd_pd(ra1, rx3, ry1);
             ry2 = _mm256_fmadd_pd(ra2, rx3, ry2);
             ry3 = _mm256_fmadd_pd(ra3, rx3, ry3);
-
 
             ptr_a += inner_unwinding;
             ptr_x += inner_unwinding;
